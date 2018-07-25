00121f228c553cbb71d5ea7ef422937f7e613383
github username <github email address>
2018-07-25 11:18:15 -0500

    mock code challenge

--- author.rb
+++ author.rb
@@ -1,1 +1,48 @@
+class Author
 
+  attr_accessor :book, :name
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def books
+    Book.all.select do |book|
+      if book.author == self.name
+        book
+      end
+    end
+
+  end
+
+  def write_book(title, word_count)
+    Book.new(self.name, title, word_count)
+  end
+
+  def total_words
+    count = 0
+    self.books.each do |book|
+      count += book.word_count
+    end
+    count
+  end
+
+  # def self.most_words
+  #   total_each = Author.all.map do |author|
+  #     self.total_words
+  #   end
+  #   total_each.find do |x|
+  #     binding.pry
+  #   end
+  # end
+
+
+
+
+end
--- console.rb
+++ console.rb
@@ -2,6 +2,22 @@ require 'pry'
 require_relative './book'
 require_relative './author'
 
+edgar = Author.new("Edgar Allen Poe")
+maya = Author.new("Maya Angelou")
+
+and_still = Book.new("Maya Angelou", "And Still I Rise", 50)
+the_raven = Book.new("Edgar Allen Poe", "The Raven", 1140)
+
+i_know_why = maya.write_book("i know why the caged bird sings", 88740)
+black_cat = edgar.write_book("The Black Cat", 3998)
+
+maya.books
+edgar.books
+maya.total_words
+edgar.total_words
+
 binding.pry
 
-
+Book.all
+Author.all
+#Author.most_words
