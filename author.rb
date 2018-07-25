a87a309d34f1a162a77b22bdc9bb28036b11fb2a
github username <github email address>
2018-07-25 11:18:08 -0500

    mock challenge

--- author.rb
+++ author.rb
@@ -1,1 +1,41 @@
+require 'pry'
+require_relative './book'
+require_relative './author'
 
+class Author
+
+  @@all = []
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def write_book(title, words)
+    new_book = Book.new(title, self.name, words)
+  end
+
+  def books
+    Book.all.select do |book|
+      book.author == self
+    end
+  end
+
+  def total_words
+    words_written = 0
+    books.each do |book|
+      words_written += book.words
+    end
+  end
+
+  # def self.most_words
+  #   self.total_words
+  #
+  # end
+
+end
--- book.rb
+++ book.rb
@@ -1,1 +1,33 @@
+require 'pry'
+require_relative './book'
+require_relative './author'
 
+class Book
+
+  attr_accessor :title, :author, :words
+  @@all = []
+
+  def initialize(title, author, words)
+    @title = title
+    @author = author
+    @words = words
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def author
+    p self.author
+  end
+
+  def title
+    p self.title
+  end
+
+  def word_count
+    p self.words
+  end
+
+end
