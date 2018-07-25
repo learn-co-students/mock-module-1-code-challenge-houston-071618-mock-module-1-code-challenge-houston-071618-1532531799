f684becfe5d1cc36dbeb22858973ac530c9508f4
RebGov <becci.govert@gmail.com>
2018-07-25 11:18:18 -0500

    not all working, yet

--- book.rb
+++ book.rb
@@ -1,1 +1,19 @@
+class Book
+  attr_accessor :title, :author, :word_count
+  @@all = []
+  def initialize(title, author)
+    @title = title
+    @author = author
+    @word_count = 0
+    @@all <<self
+  end
 
+  def self.all
+    @@all
+  end
+
+
+
+
+
+end
--- console.rb
+++ console.rb
@@ -1,7 +1,13 @@
 require 'pry'
-require_relative './book'
-require_relative './author'
+require_relative 'book'
+require_relative 'author'
+
 
 binding.pry
+book1 = Book.new("Book1", "A Name")
+book2 = Book.new("Book2", "B Name")
+book3 = Book.new("Book3", "C Name")
+book4 = Book.new("Book4", "A Name")
 
-
+authorA = Author.new("C Name", "Book5")
+Pry.start
