d31500633769ed58050386ac8b044e1676687d96
github username <github email address>
2018-07-25 10:50:59 -0500

    all methods built and tested

--- author.rb
+++ author.rb
@@ -1,1 +1,48 @@
+class Author
 
+  attr_reader :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def write_book(book_title, book_word_count)
+    new_book = Book.new(title: book_title, word_count: book_word_count, author: self)
+    self.books << new_book
+  end
+
+  def books
+    Book.all.select do |book_obj|
+      book_obj.author == self
+    end
+  end
+
+  def total_words
+    total_words = 0
+    self.books.each do |book_obj|
+      total_words += book_obj.word_count
+    end
+    total_words
+  end
+
+  def self.most_words
+    most_words = 0
+    most_words_author_obj = {}
+
+    Book.all.each do |book_obj|
+      if book_obj.word_count > most_words
+        most_words = book_obj.word_count
+        most_words_author_obj = book_obj.author
+      end
+    end
+
+    most_words_author_obj
+  end
+end
--- console.rb
+++ console.rb
@@ -2,6 +2,19 @@ require 'pry'
 require_relative './book'
 require_relative './author'
 
+isaac_asimov = Author.new("asimov")
+robert_caro = Author.new("caro")
+dan_ariely = Author.new("ariely")
+
+isaac_asimov.write_book("iRobot", 250)
+isaac_asimov.write_book("The Foundation", 450)
+robert_caro.write_book("The Power Broker", 1200)
+robert_caro.write_book("The Path To Power", 700)
+dan_ariely.write_book("Predictibly Irrational", 250)
+
 binding.pry
+robert_caro.books
 
+isaac_asimov.total_words
 
+Author.most_words
