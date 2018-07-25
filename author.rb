7a7ac1705f80f594d9d87fe962fe0ea4712b587f
pabloargueta <pabloargueta27@gmail.com>
2018-07-25 11:00:19 -0500

    Done

--- author.rb
+++ author.rb
@@ -1,1 +1,47 @@
+class Author
 
+  @@all = []
+
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+    Author.all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def books
+    Book.all.select do |book|
+      book.author == self
+    end
+  end
+
+  def write_book(title, word_count)
+    Book.new(title, self, word_count)
+  end
+
+  def total_words
+    total = 0
+    self.books.each do |book|
+      total += book.word_count
+    end
+    total
+  end
+
+  def self.most_words
+    words = 0
+    author_name = ''
+    self.all.each do |author|
+      author_words = author.total_words
+      if words < author_words
+        words = author_words
+        author_name = author.name
+      end
+    end
+    author_name
+  end
+
+end
