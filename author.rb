bce058206138f92c29155a759ba5709e5186f2ac
Scott Tang <fq600@yahoo.com>
2018-07-25 11:17:39 -0500

    done

--- author.rb
+++ author.rb
@@ -1,1 +1,45 @@
 
+class Author
+  @@all = []
+  @@all_word_count = []
+
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def books
+    Book.all.select do |book|
+      book.author == self
+    end
+  end
+
+  def self.all
+    @@all
+  end
+
+  def write_book(title, word_count)
+    Book.new(title, self, word_count)
+  end
+
+  def total_words
+    count = 0
+    books.map do |book|
+      count += book.word_count
+    end
+    @@all_word_count << count
+    count
+  end
+
+  def self.most_words
+    all_word_count.max
+  end
+
+  private
+
+  def self.all_word_count
+    @@all_word_count
+  end
+end
--- console.rb
+++ console.rb
@@ -1,7 +1,25 @@
-require 'pry'
-require_relative './book'
-require_relative './author'
+require "pry"
+require_relative "./book"
+require_relative "./author"
 
-binding.pry
+scott = Author.new("Scott Tang")
+james = Author.new("James Kim")
 
+fail_forward = Book.new("Failing Forward", scott, 10)
+reef_keeping = Book.new("Reef Keeping", james, 90000)
 
+p fail_forward.word_count
+p reef_keeping.word_count
+p fail_forward.author.name
+
+# p scott.books
+# p Author.all
+
+p "write books"
+p scott.write_book("Ruby Programming", 1200)
+p scott.total_words
+p james.total_words
+
+p "most words"
+
+p Author.most_words
