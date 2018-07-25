7652e474e255aa2cef088364a871035453912a8c
github username <github email address>
2018-07-25 11:18:25 -0500

    finished

--- author.rb
+++ author.rb
@@ -1,1 +1,36 @@
+require 'pry'
+class Author
 
+@@all = []
+attr_accessor :books
+
+def initialize (books)
+  @books = books
+  self.all << self
+end
+
+def self.all
+  @@all
+end
+
+def books
+  self.all.select do |authors|
+  authors == self
+ end
+hash = {}
+hash[:self] = books
+end
+
+#def write_book (title, word_count)
+#books
+
+#end
+
+#def total_words
+#  self.books.
+#end
+
+#def self.most_words
+#end
+
+end
--- book.rb
+++ book.rb
@@ -1,1 +1,32 @@
+require 'pry'
+class Book
 
+  @@all = []
+
+attr_reader :author, :title, :word_count
+
+def initialize (author, title, word_count)
+  @author = author
+  @title = title
+  @word_count = word_count
+  self.all << self
+end
+
+def self.all
+@@all
+end
+
+  def author
+    @author
+end
+
+def title
+  @title
+end
+
+def word_count
+  #should return the number of words in the book
+@word_count
+end
+
+end
