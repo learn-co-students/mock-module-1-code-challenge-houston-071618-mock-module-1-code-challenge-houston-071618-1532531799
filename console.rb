2943a5e27c2f095c3c66e0f283376b2ccf0b27dc
pandasandeggs <theteatreehouse@gmail.com>
2018-07-25 11:18:45 -0500

    mock code challenge

--- console.rb
+++ console.rb
@@ -2,6 +2,10 @@ require 'pry'
 require_relative './book'
 require_relative './author'
 
+eragon = Book.new("Eragon")
+chris = Author.new("Christopher P")
+
+eragon.author
+chris.books
+
 binding.pry
-
-
