00279f91705ffcb72c46527c5d94b03c248c811f
github username <github email address>
2018-07-25 11:18:06 -0500

    submitting mock test

--- console.rb
+++ console.rb
@@ -2,6 +2,23 @@ require 'pry'
 require_relative './book'
 require_relative './author'
 
+
+jk = Author.new("JK Rowling")
+hp = Book.new("Harry Potter 1", 300, jk)
+
+abc = Author.new("abc")
+xyz = Book.new("xyz", 200, abc)
+
+jk.write_book("HP2", 500)
+# jk.books
+
+hp.author
+
+hp.title  # Book#title should return the title of the book
+hp.word_count   # Book#word_count should return the number of words in the book
+
 binding.pry
 
+jk.total_words
 
+# Author.most_words # Author.most_words should return the author instance who has written the most words
