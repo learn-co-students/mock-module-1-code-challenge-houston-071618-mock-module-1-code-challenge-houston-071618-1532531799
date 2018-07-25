61c3d81e812f6f56b2af742229e1af47c1546bb4
kevinptx <kevin.peery@gmail.com>
2018-07-25 11:08:29 -0500

    mock challenge completed

--- author.rb
+++ author.rb
@@ -1,1 +1,36 @@
+require "pry"
 
+class Author
+  attr_accessor :author, :title
+  @@all = []
+
+  def initialize()
+    @title = title
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def books
+    Author.all.select do |book|
+      book.author == self
+    end
+  end
+
+  def write_book(title, word_count)
+    Book.new(author)
+  end
+
+  def total_words
+    Book.all.reduce(0) do |x, y|
+      x.author + y.author
+    end
+  end
+
+  #this is a class method as a . is used
+  #therefore we call self as we refer to the current class of Author
+  def self.most_words
+  end
+end
--- book.rb
+++ book.rb
@@ -1,1 +1,34 @@
+require "pry"
 
+class Book
+  attr_accessor :author, :title
+
+  @@all = []
+
+  #a book would need arguments of author, title
+  def initialize(author, title)
+    @author = author
+    @title = title
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def author
+    Book.all.find do |book|
+      book.author == self
+      binding.pry
+    end
+  end
+
+  def title(title)
+    Book.all.find do |book|
+      book.title == self
+    end
+  end
+
+  def word_count
+  end
+end
