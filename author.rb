1146df21894f13f52d536f55b76e29b5d8595a36
github username <github email address>
2018-07-25 10:48:41 -0500

    Done.

--- author.rb
+++ author.rb
@@ -1,1 +1,29 @@
+class Author
+  @@all = []
+  attr_reader :name
 
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def books
+    Book.all.select { |book| book.author == self }
+  end
+
+  def write_book(title, word_count)
+    Book.new(self, title, word_count)
+  end
+
+  def total_words
+    books.map { |book| book.word_count }.reduce(:+)
+  end
+
+  def self.most_words
+    self.all.sort_by { |author| author.total_words }.reverse.first
+  end
+end
--- console.rb
+++ console.rb
@@ -2,6 +2,10 @@ require 'pry'
 require_relative './book'
 require_relative './author'
 
+jkr = Author.new("JKR")
+jrrt = Author.new("JRRT")
+hp = Book.new(jkr, "HP 1", 10000)
+hp2 = Book.new(jkr, "HP 2", 20000)
+lotr = Book.new(jrrt, "LOTR", 20000)
+
 binding.pry
-
-
