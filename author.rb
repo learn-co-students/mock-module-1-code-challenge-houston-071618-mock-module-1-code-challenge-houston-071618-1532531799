386e3b28445345e1f5cdf32a9a68a70509b9b31e
github username <github email address>
2018-07-25 11:21:39 -0500

    first

--- author.rb
+++ author.rb
@@ -1,1 +1,45 @@
+class Author
 
+  @@all = []
+
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def books
+    Book.all.select do |book|
+      book.author == self.name
+    end
+  end
+
+  def write_book(title, word_count)
+    new_book = Book.new(title, self, word_count)
+  end
+
+  def total_words
+    num_array = self.books.map do |book|
+      book.word_count
+    end
+    total = num_array.inject(0){|sum,x| sum + x }
+  end
+
+  # def self.word_totals
+  #   self.all.map do |author|
+  #     author.total_words
+  #   end
+  # end
+
+  def self.most_words
+    Book.all.max_by do |book|
+      book.word_count
+    end
+  end
+
+end
--- console.rb
+++ console.rb
@@ -2,6 +2,11 @@ require 'pry'
 require_relative './book'
 require_relative './author'
 
+jack = Author.new("Jack")
+suess = Author.new("Dr. Suess")
+on_the_road = Book.new("On The Road", "Jack", 200)
+green_eggs = Book.new("Green Eggs and Ham", "Dr. Suess", 25)
+another_one = Book.new("Another One", "Jack", 200)
+jack.total_words
+
 binding.pry
-
-
