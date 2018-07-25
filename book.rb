0b73df88decc93ccc54379602ec2c6802cf0ce97
Ryhan Chowdhury <ryhanchowdhury@apples-MacBook-Pro.local>
2018-07-25 11:17:49 -0500

    Submitting mock code challenge

--- book.rb
+++ book.rb
@@ -1,1 +1,27 @@
+class Book
 
+  @@all=[]
+
+  def initialize(author, title, word_count)
+    @author = author
+    @title = title
+    @word_count = word_count
+    @@all.push(self)
+  end
+
+  def author #needs to return author instance
+    return @author
+  end
+
+  def title
+    return @title
+  end
+
+  def word_count
+    return @word_count
+  end
+
+  def self.all
+    return @@all
+  end
+end
